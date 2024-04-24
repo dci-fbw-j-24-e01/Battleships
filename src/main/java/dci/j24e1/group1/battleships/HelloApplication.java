fc5e73124939f6686f4baa2a351a96eeb3c5a7d5
Herr Oleh Bryl <olegbryl@gmail.com>
2024-04-24 15:11:23 +0200

    WTF2

--- src/main/java/dci/j24e1/group1/battleships/HelloApplication.java
+++ src/main/java/dci/j24e1/group1/battleships/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+//testing
     public static void main(String[] args) {
         launch();
     }
